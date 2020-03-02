6aa3ace83309f650f9fc7599bd71ab6d36cd246d
Arthur Kalikiti <arthurkalikiti@gmail.com>
2020-03-02 08:09:45 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="HomeComponent">
+      <div class="head">
+          <h1> {{ heading }} </h1>
+          <br />
+          <p> {{ msg }} </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String,
+    heading: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
